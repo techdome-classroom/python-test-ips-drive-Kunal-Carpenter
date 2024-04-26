9ce4f8bdb9b8e5f4cf9f25b60ae1607ad5448241
Kunal-Carpenter <168173811+Kunal-Carpenter@users.noreply.github.com>
2024-04-26 13:53:59 +0530

    Update program2.py

--- program2.py
+++ program2.py
@@ -8,4 +8,19 @@ def longest_substring(s: str) -> int:
 
     """ 
     pass
+def longest_substring(s):
+    if not s:
+        return 0
 
+    char_index = {}
+    max_length = 0
+    start = 0
+
+    for end in range(len(s)):
+        if s[end] in char_index and char_index[s[end]] >= start:
+            start = char_index[s[end]] + 1
+        else:
+            max_length = max(max_length, end - start + 1)
+        char_index[s[end]] = end
+
+    return max_length
